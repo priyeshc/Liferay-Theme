f1deb5037030f835951c4237334fe9b979dea7a4
Priyesh Chandra <priyesh.chandra@argildx.net>
2022-01-11 12:10:44 +0530

    first commit

---
+++ portlet-filter/src/main/java/com/liferay/tasks/portlet/filter/constants/PortletFilterPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.tasks.portlet.filter.constants;
+
+/**
+ * @author Argil DX
+ */
+public class PortletFilterPortletKeys {
+
+	public static final String PORTLETFILTER =
+		"com_liferay_tasks_portlet_filter_PortletFilterPortlet";
+
+}
